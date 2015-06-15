39cac0247a0b05522f4ba55e88eb6c34d1d1e968
D.J. Dykes <ddykes2@gmail.com>
2015-06-15 02:06:38 -0700

    Implemented decryption and commandline interface.

--- README.txt
+++ README.txt
@@ -1,6 +1,11 @@
-This is a simple Caesar shift-cipher by D.J. Dykes
+This is a simple Caesar shift-cipher and decoder by D.J. Dykes
 
 To run:
-Checkout and run with <Your Text File Here> as the first argument.
 
-ex$ ruby caesar.rb sample.txt
+    ARGV 1 = your filename
+    ARGV 2 = '#' to shift by -OR- 'decrypt' to bring up decryption menus
+        
+Examples for your convenience:
+
+    ex$ ruby caesar.rb sample.txt 420     # => shift by 420 
+    ex$ ruby caesar.rb sample.txt decrypt  # => decrypt this file
--- caesar.rb
+++ caesar.rb
@@ -1,7 +1,20 @@
 class Caesar
   def initialize
-    @text_in = open(ARGV.first)
-    #@shift = ARGV.second
+    @text_in = open(ARGV[0])
+
+    if ARGV[1] == 'decrypt'
+      puts "Enter decryption key or type 'brute' to employ brutus force"
+      ans = $stdin.gets.chomp
+      if ans == 'brute' # enter brute force
+        puts "\n et tu...? \n"
+        brutus()
+      else
+        cyghfer(ans.to_i * -1) # proceed with decrypt in the opposite direction 
+        # TODO: come up with a way to make this more explicit?
+      end 
+    else # proceed with encrypt
+      cyghfer(ARGV[1].to_i) 
+    end
   end
 
   def cyghfer(shift)    
@@ -22,14 +35,39 @@ class Caesar
     end
 
     # Visual Output
+    puts ' '
     puts '=' * 69
       puts '100% ULTRA TOP SECERET CODED MESSAGE #2015 #EXTRAVAGANZA'    
     puts '=' * 69
     puts output.join + "[ #{shift} ]"
-    puts ' '
+    # puts written to >FILENAME GOES HERE<
   end
 
-# NOTE: Super-mega interesting: While Ruby does interpret negative calls on indices to originate from the END of the array, it doesn't wrap around if the call is greater than +/- the total number of entries. So, to keep the text safe from having NIL values injected into it by large shifts, here is a wrapping algorithm to wrangle those integers back into their box.
+  def brutus()
+    isdone = false 
+    newshift = 1
+
+    while isdone == false
+      cyghfer(newshift)
+      puts "Are we there yet? [y/n] \n"
+      answer = $stdin.gets.chomp.to_s
+      if answer == 'y'
+        puts "\nEXCELSIOR!!!!\n"
+        isdone = true
+      else answer == 'n'
+        newshift += 1  #TODO:*** WHEN YOU IMPLEMENT WRITE TO FILE, DELETE THIS LINE***
+        puts "Let's try again..."
+        @text_in = open(ARGV[0])
+      end
+    end
+  end
+
+# NOTE: Super-mega interesting: While Ruby does interpret negative calls on indices
+# to originate from the END of the array, it doesn't wrap around if the call is 
+# greater than +/- the total number of entries. So, to keep the text safe from having
+# NIL values injected into it by large shifts, here is a wrapping algorithm to wrangle
+# those integers back into their box.
+  
   def wrap(x)  
     range = 26
     if (x < 0)
@@ -38,11 +76,5 @@ class Caesar
     x = (x % range)
   end
 end
-
+ 
 start = Caesar.new
-#start.cyghfer(0)
-#start.cyghfer(1)
-#start.cyghfer(-1)
-#start.cyghfer(26)
-#start.cyghfer(-26)
-start.cyghfer(999999999)
